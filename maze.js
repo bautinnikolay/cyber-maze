de9028ff1bcc7fff7e2c1d97a68b215daab70c17
bautinnikolay <bautinnikolay@gmail.com>
2018-12-10 23:30:24 +0500

    add isNeedAWall function

--- maze.js
+++ maze.js
@@ -1,5 +1,5 @@
-let width = 20
-let height = 20
+let width = 100
+let height = 100
 
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -25,7 +25,7 @@ map[b[0]][b[1]] = 2
 let sum = [a, b]
 
 function getOne(arr) {
-    if(arr.length > 0) {
+    while(arr.length > 0) {
         let cell = 0
         if(arr.length > 1) {
             cell = getRandom(0, arr.length)
@@ -36,23 +36,31 @@ function getOne(arr) {
                     if(map[arr[cell][0]+x][arr[cell][1]+y] == 0) {
                         map[arr[cell][0]+x][arr[cell][1]+y] = map[arr[cell][0]][arr[cell][1]]
                         sum.push([arr[cell][0]+x, arr[cell][1]+y])
-                    }   
+                    }
                 }
             }
         }
         arr.splice(cell, 1)
-        getOne(arr)
     }
 }
 
-/*function getRegion(arr) {
-	let cellIn = 0
-	for(let x = 0; x < arr.length; x++) {
-		for(let y = 0; y < arr[0].length; y++) {
-
-		}
-	}
-} */
+function isNeedAWall(x, y) {
+  let result = true
+  if( x % 2 == 0 && y % 2 == 0) {
+    if(map[x/2-1][y/2-1] == map[x/2][y/2-1] && map[x/2][y/2-1] == map[x/2][y/2] && map[x/2][y/2] == map[x/2-1][y/2] && map[x/2-1][y/2] == map[x/2-1][x/2-1]) {
+      result = false
+    }
+  } else if (x % 2 == 0 && y % 2 != 0) {
+    if(map[x/2][y/2] == map[x/2-1][y/2]) {
+      result = false
+    }
+  } else if (x % 2 != 0 && y % 2 == 0) {
+    if(map[x/2][y/2] == map[x/2][y/2-1]) {
+      result = false
+    }
+  }
+  return result
+}
 
 getOne(sum)
 console.log(map)
